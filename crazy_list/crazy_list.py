0fba1a8a16d8dfe4efc69aab8398130283426747
mohakhalili71 <md.kh71@gmail.com>
2019-07-30 19:37:07 +0430

    crazy_listis added

---
+++ crazy_list/crazy_list.py
@@ -0,0 +1,11 @@
+def crazy_list(MyList):
+    Condition = True
+    for i in range(len(MyList)):
+        if MyList[i] != MyList[-i-1]:
+            Condition = False 
+    
+    return Condition
+
+listnew = [4, 5, 6, 7, 8, 4, 5, 2]
+x = crazy_list(listnew)
+print(x)
