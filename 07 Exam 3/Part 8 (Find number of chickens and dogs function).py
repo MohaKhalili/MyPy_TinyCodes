5586d1f6a762e7bc55210cdce2a64a60d3cb9fc9
mohakhalili71 <md.kh71@gmail.com>
2019-08-26 19:31:07 +0430

    Exam 3 (part 8-v2) is added

--- 07 Exam 3/Part 8 (Find number of chickens and dogs function).py
+++ 07 Exam 3/Part 8 (Find number of chickens and dogs function).py
@@ -1,18 +1,43 @@
-# a function that accepts two positive integers as function parameters and returns their least 
-# common multiple (LCM). The LCM of two integers a and b is the smallest (non zero) positive 
-# integer that is divisible by both a and b. For example, the LCM of 4 and 6 is 12, the LCM of 10 and 5 is 10.
+# a function that accepts two positive integers as parameters. The first integer is the number 
+# of heads and the second integer is the number of legs of all the creatures in a farm which 
+# consists of chickens and dogs. Your function should calculate and return the number of 
+# chickens and number of dogs in the farm in a list as specified below. If it is impossible 
+# to determine the correct number of chickens and dogs with the given information then your 
+# function should return None. 
 
-# Remember that you are not asked to print anything. So, your function should return the LCM and not print it. 
-# You do not need to call your function, it will automatically be called and tested for correctness with the 
-# test cases we provide. You only need to write one function and we will test your program with the first 
-# function that appears in your code. So, if you want to write more than one function to help you solve the 
-# problem, remember to embed the helper functions inside the first function. 
+# Example 1, if your function received the following numbers:
+# 5, 12 
+# This means that someone has counted a total of 5 heads and total of 12 legs in the farm. 
+# Now, your function should calculate how many chickens and how many dogs are in the farm 
+# and return that information in a list exactly as shown below.
+# [4, 1] 
+# this means that there were 4 chickens and 1 dog in the farm.
+
+# Example 2, if your function received the following numbers:
+# 7, 12 
+# Then it should return:
+# None 
+
+
+
+# Remember that you are not asked to print anything. So, your function should return a list that 
+# contains two numbers exactly in this order [number_of_chickens, number_of_dogs] not print it. 
+# You do not need to call your function, it will automatically be called and tested for correctness
+# with the test cases we provide. You only need to write one function and we will test your program 
+# with the first function that appears in your code. So, if you want to write more than one function 
+# to help you solve the problem remember to embed the helper functions inside the first function.
 
 # Type your code here
-def LCM(a,b):
-    def GCD(a,b):
-        if a == 0: 
-            return b 
-        return GCD(b % a, a)
-    result = (a*b) / GCD(a,b)
+def CountAnimal(Heads, Legs): 
+    Dog = 0
+    Dog = (Legs) - 2 * (Heads)
+    if Dog < 0: 
+        result = None
+    else:
+        Dog = Dog / 2
+        Chicken = Heads - Dog
+        if Chicken < 0: 
+            result = None
+        else:
+            result = [int(Chicken), int(Dog)]        
     return result
