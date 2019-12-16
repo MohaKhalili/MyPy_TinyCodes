e14c2016b54ccc78c9593b17afe5c428930405b2
mohakhalili71 <md.kh71@gmail.com>
2019-12-16 16:57:02 +0330

    Exercise two is edited

--- 00 - Week 1/02 Python Basics_Exercise 2.py
+++ 00 - Week 1/02 Python Basics_Exercise 2.py
@@ -1,6 +1,40 @@
-# What will be printed after each of the following code segments?
-# If error, then write Error
+# What will be printed after each of the following code 
+# segments? If error, then write Error
 
-# Pay attention to the spaces. Your answer should exactly match the
-# correct Python output.
+# Pay attention to the spaces. Your answer should exactly 
+# match the correct Python output.
+#############################################################
+# x = 2.0
+# y = 1
+# z=x+y
+# print(z)
 
+# answer : 3.0
+#############################################################
+# x = 14 / 2
+# print(x)
+
+# answer : 7.0
+#############################################################
+# x = (24 * 2) / 3
+# y = 2.5
+# print(x + (y * 2))
+
+# answer : 21.0
+#############################################################
+# x = "hello there"
+# print(x)
+
+# answer : hello there
+#############################################################
+# x = "John"
+# print("hello",x)
+
+# answer : hello John
+#############################################################
+# x = "dog"
+# y = "cat"
+# x = 2
+# print(x, y)
+
+# answer : 2 cat
