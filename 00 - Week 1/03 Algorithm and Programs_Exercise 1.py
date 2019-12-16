cd23c7a5944a9ffb1ca15b861ca963fa777d5a5f
mohakhalili71 <md.kh71@gmail.com>
2019-12-16 16:57:37 +0330

    Exercise three is edited

--- 00 - Week 1/03 Algorithm and Programs_Exercise 1.py
+++ 00 - Week 1/03 Algorithm and Programs_Exercise 1.py
@@ -1,1 +1,14 @@
 # Which one of the following statements is NOT correct?
+
+# 1. An algorithm is a description of how to solve a problem.
+# 2. An algorithm is a step by step recipe of how to perform a task.
+# 3. Algorithm is another name for programing languages. (correct)
+
+
+################################################################
+
+# Which one of the following statements is NOT correct?
+
+# 1. A program is an implementation of an algorithm in a specific programming language.
+# 2. A program is a sequence of instructions to a computer.
+# 3. A program can be written independent of a programming language. (correct)
