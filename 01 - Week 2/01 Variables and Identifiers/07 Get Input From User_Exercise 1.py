f3ab3a20253ec6e69a4eaac051095a868476156c
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 17:15:57 +0330

    The question is added

--- 01 - Week 2/01 Variables and Identifiers/07 Get Input From User_Exercise 1.py
+++ 01 - Week 2/01 Variables and Identifiers/07 Get Input From User_Exercise 1.py
@@ -1,1 +1,10 @@
 # For each of the following variables, what is the type?.
+
+# x = input("What is your name?")
+# str
+
+# y = input("What is your age?")
+# str
+
+# z = int(input("how many children do you have?"))
+# int
