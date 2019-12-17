78eb8f05a65ef5935d0ff4a52257ca6db05678ee
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 17:53:24 +0330

    Ex3 for List is added

--- 01 - Week 2/03  List Basics/Exercise3.py
+++ 01 - Week 2/03  List Basics/Exercise3.py
@@ -1,1 +1,17 @@
 # List Basics, Exercise 3 
+
+# What will be printed after each of the following code segments? If error, then write Error
+
+# x = [5, 'dog']
+# y = ['cat', 3.5]
+# print(x + y)
+# Answer : [5, 'dog', 'cat', 3.5]
+
+# x = [ 5, 'dog']
+# print(3 * x)
+# Answer : [5, 'dog', 5, 'dog', 5, 'dog']
+
+# x = [5, 'dog']
+# y = ['cat']
+# print(x + 2 * y)
+# Answer : [5,'dog','cat','cat']
