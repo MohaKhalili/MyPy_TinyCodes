878916068db1f54eeafb5cc4d43707287e8e862c
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 14:44:09 +0330

    The questions is added

--- 01 - Week 2/01 Variables and Identifiers/03 How to Name Your Identifiers_Exercise 2.py
+++ 01 - Week 2/01 Variables and Identifiers/03 How to Name Your Identifiers_Exercise 2.py
@@ -1,1 +1,15 @@
 # Determine if the following variable names are valid or invalid:
+
+# Which of the following variable names is valid in Python?
+# 2_apple
+# 2-apple
+# two_apple (correct)
+# two-apple
+
+#############################################################
+
+# Which of the following variable names is NOT valid in Python?
+# name
+# width
+# for (correct)
+# length 
