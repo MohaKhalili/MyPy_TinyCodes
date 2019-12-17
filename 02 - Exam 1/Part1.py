dfd1acf3df2cd1e149250b7430dfb0d194117308
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 18:02:55 +0330

    Part1 is added

--- 02 - Exam 1/Part1.py
+++ 02 - Exam 1/Part1.py
@@ -1,1 +1,30 @@
 # This part includes 8  tiny questions
+
+
+# For each of the following variables, what is the type?
+
+# x = (5+4)/3 
+# Answer : float
+
+# y = "hello there"
+# Answer : str
+
+# z = True 
+# Answer : bool
+
+# x = int(7/2) 
+# Answer : int
+
+# For the following questions, determine if the following variable names are valid or invalid:
+
+# 2_list
+# Answer : Invalid
+
+# height_
+# Answer : Valid
+
+# width@
+# Answer : Invalid
+
+# if
+# Answer : Invalid 
