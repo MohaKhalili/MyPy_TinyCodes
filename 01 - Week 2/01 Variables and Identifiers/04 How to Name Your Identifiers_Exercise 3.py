c9fc736f1308fb7023254c3f60fe1531573d16c0
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 14:47:01 +0330

    The question is added

--- 01 - Week 2/01 Variables and Identifiers/04 How to Name Your Identifiers_Exercise 3.py
+++ 01 - Week 2/01 Variables and Identifiers/04 How to Name Your Identifiers_Exercise 3.py
@@ -3,3 +3,35 @@
 
 # Pay attention to the spaces. Your answer should exactly match the
 # correct Python output.
+
+# Y = 2.3
+# y = "dog"
+# print(Y)
+
+# answer : 2.3
+###################################################
+# M = 12
+# print(m)
+
+# answer : Error
+###################################################
+# 2x = 4
+# print(2x)
+
+# answer : Error
+###################################################
+# my_variable = 4
+# print(my_variable)
+
+# answer : 4
+###################################################
+# my variable = 5
+# print(my variable)
+
+# answer : Error
+###################################################
+# A_2 = "HELLO"
+# print(A_2)
+
+# answer : HELLO
+###################################################
