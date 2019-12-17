e2a6fb847ac96032251f7ab46f3c00e20afc903e
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 14:33:31 +0330

    The questions is added

---
+++ 01 - Week 2/01 Variables and Identifiers/01 Variables and Identifier_Exercise1.py
@@ -0,0 +1,13 @@
+
+# Which of the following is NOT correct?
+
+# An identifier is the name given to a variable, a function or an object.
+# In python different data types can be stored in a variable.
+# A variable is the name given to an identifier. (correct)
+
+#############################################################
+# Which of the following is NOT correct?
+
+# A variable has properties such as value, type, and scope.
+# Variables and identifiers are the same. (correct)
+# A variable is a reserved location in a computer memory to store data. 
