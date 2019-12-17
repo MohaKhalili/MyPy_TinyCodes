ee5dbe2777b82bde28118f44e7f3af7f31ba71f1
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 16:06:21 +0330

    The questions is added

--- 01 - Week 2/01 Variables and Identifiers/06 Variable Types_Exercise 2.py
+++ 01 - Week 2/01 Variables and Identifiers/06 Variable Types_Exercise 2.py
@@ -1,1 +1,16 @@
 # For each of the following variables, what is the type?
+
+# x = (5 + 4) 
+# int
+
+# z = (6.2 - 2.2)
+# float
+
+# y = 'university' 
+# str
+
+# z = 8 / 2
+# float
+
+# x = True
+# bool
