435a408a454343f5fa6029cc8c8b0a82e5a4f5e4
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 17:48:52 +0330

    Ex1 for List is added

--- 01 - Week 2/03  List Basics/Exercise1.py
+++ 01 - Week 2/03  List Basics/Exercise1.py
@@ -1,1 +1,20 @@
 # Lists, Exercise 1
+
+# What will be printed after each of the following code segments? If error, then write Error
+# Pay attention to the spaces. Your answer should exactly match the correct Python output.
+
+# my_list = [5, 'old', 'new', 8, 'time', 2]
+# print(my_list[0])
+# Answer : 5
+
+# my_list = [5, 'old', 'new', 8, 'time', 2]
+# print(my_list[2])
+# Answer : new
+
+# my_list = [5, 'old', 'new', 8, 'time', 2]
+# print(my_list[4])
+# Answer : time
+
+# my_list = [5, 'old', 'new', 8, 'time', 2]
+# print(my_list[6])
+# Answer : Error
