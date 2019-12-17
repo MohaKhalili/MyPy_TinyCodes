ea959305179bd9730dfbb34a7ce682aeb26ef2c5
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 17:21:46 +0330

    calculate area & perimeter of Rectangle is added

---
+++ 01 - Week 2/Ex_perimeter_&_area.py
@@ -0,0 +1,13 @@
+# A program to calculate area & perimeter of Rectangle
+# this is written on 98 tir
+
+length_Rectangle = 4 # input the length of Rectangle
+width_Rectangle = 3 # input the width of Rectangle
+
+# do the calculation
+area_Rectangle = length_Rectangle * width_Rectangle
+perimeter_Rectangle = (length_Rectangle + width_Rectangle)*2
+
+# print the output
+print("area of the rectangle is: ",area_Rectangle)
+print("perimeter of the rectangle is: ",perimeter_Rectangle)
