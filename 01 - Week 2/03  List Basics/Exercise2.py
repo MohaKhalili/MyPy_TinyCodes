d87da0c62652af68e5dc1682704e06bf85369346
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 17:51:01 +0330

    Ex2 for List is added

--- 01 - Week 2/03  List Basics/Exercise2.py
+++ 01 - Week 2/03  List Basics/Exercise2.py
@@ -1,1 +1,20 @@
-# List Basics, Exercise 2 
+# List Basics, Exercise 2
+
+# What will be printed after each of the following code segments? If error, then write Error
+# Pay attention to the spaces. Your answer should exactly match the correct Python output.
+
+# my_list = [2.3, 'dog', 34, 'cat', 52]
+# print(my_list[-1]) 
+# Answer : 52
+
+# my_list = [2.3, 'dog', 34, 'cat', 52]
+# print(my_list[-5])
+# Answer : 2.3
+
+# my_list = [2.3, 'dog', 34, 'cat', 52]
+# print(my_list[-2])
+# Answer : cat
+
+# my_list = [2.3, 'dog', 34, 'cat', 52]
+# print(my_list[-6])
+# Answer : Error
