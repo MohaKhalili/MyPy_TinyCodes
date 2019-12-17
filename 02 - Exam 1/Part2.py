37bc2a0d18bfca734db4c56f90d94c3c00bfc684
mohakhalili71 <md.kh71@gmail.com>
2019-12-17 18:07:50 +0330

    Part 2 is added

--- 02 - Exam 1/Part2.py
+++ 02 - Exam 1/Part2.py
@@ -1,1 +1,53 @@
 # This part includes 12 tiny questions
+
+# What will be printed after each of the following code segments? If error, then write Error
+
+# Pay attention to the spaces. Your answer should EXACTLY match the correct Python output.
+
+# print((15 / 5)* 2)
+# Answer : 6.0
+
+# x = 4
+# print(x * 2)
+# Answer : 8
+
+# x = (int(4.2)/2)
+# print(x)
+# Answer : 2.0
+
+# print(int(15/2)+3)
+# Answer : 10
+
+# print(2,3)
+# Answer : 2 3
+
+# print("hello",      5)
+# Answer : hello 5
+
+# x = 5
+# print("x is", x   ,    ".")
+# Answer : x is 5 .
+
+# x = 5
+# y = 9
+# print("x is", y,"y is",x )
+# Answer : x is 9 y is 5
+
+# my_list = ['hello', 2, 3.5, 'bye', 14]
+# print(my_list[1])
+# Answer : 2
+
+# ______________________________________________________________________________________
+# my_list = ['hello', 2, 3.5, 'bye', 14]
+# print(my_list[-3])
+# Answer : 3.5
+
+# ______________________________________________________________________________________
+# my_list = ['hello', 2, 3.5, 'bye', 14]
+# print(my_list[5])
+# Answer : Error
+
+# ______________________________________________________________________________________
+# my_list = ['hello', 2, 3.5, 'bye', 14]
+# print(my_list[1]+my_list[-1])
+# Answer : 16
