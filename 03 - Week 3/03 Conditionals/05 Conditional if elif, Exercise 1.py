7e1a5f02e26682fde95fe3ec89644849c208b57e
mohakhalili71 <md.kh71@gmail.com>
2019-12-18 22:59:30 +0330

    if elif, Ex1 added

---
+++ 03 - Week 3/03 Conditionals/05 Conditional if elif, Exercise 1.py
@@ -0,0 +1,41 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+my_list=[ 2.3, 'car', 12, 46, 'cat']
+if 12 in my_list:
+    print('hello')
+elif 6 > 4:
+    print('bye')
+else:
+    print('nothing')
+###################################################################
+if 6/2:
+    print('three')
+elif 5 :
+    print('five')
+else:
+    print('zero')
+###################################################################
+if False:
+    print('false')
+elif 2**3 == 8 :
+    print('true')
+else:
+    print('none')
+###################################################################
+my_string="hello"
+if 10 % 5:
+    print('true')
+elif "le" not in my_string :
+    print('false')
+else:
+    print('none')
+###################################################################
+if 6 <5:
+    print('one')
+elif 7 == 9 :
+    print('two')
+print("three")
