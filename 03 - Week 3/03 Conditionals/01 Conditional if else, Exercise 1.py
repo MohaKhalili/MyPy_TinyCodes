20c91753e71e332d562e521aaa1200552711b9b6
mohakhalili71 <md.kh71@gmail.com>
2019-12-18 22:32:55 +0330

    if else, Ex1 added

---
+++ 03 - Week 3/03 Conditionals/01 Conditional if else, Exercise 1.py
@@ -0,0 +1,32 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+my_string = "hello there"
+if "th" in my_string:
+    print('yes')
+else:
+    print('no')
+###################################################################
+if 5 > 8:
+    print('greater')
+else:
+    print('less')
+###################################################################
+if 0:
+    print('yes')
+else:
+    print('no')
+###################################################################
+if 6 != 8 and 3 > 1:
+    print('both')
+else:
+    print('none')
+###################################################################
+if "a" in "there" or 6 % 2:
+    print('true')
+else:
+    print('false')
+###################################################################
