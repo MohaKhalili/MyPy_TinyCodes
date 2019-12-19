165b2b54b0e5ba511a3053ca118dfebe22eef605
mohakhalili71 <md.kh71@gmail.com>
2019-12-19 15:44:57 +0330

    Part 5 edited

--- 04 - Exam 2/Part5.py
+++ 04 - Exam 2/Part5.py
@@ -22,3 +22,19 @@ else:
     DIFF = n - 50
     MONEY = (40 * 8) + (10 * 9) + (DIFF * 10)
     print("YOU MADE",MONEY, "DOLLARS THIS WEEK")
+
+################### Sample Solution ###################
+# user_response=input("Type an Integer:")
+# x = int(user_response)
+# total_amount = 0
+# if x < 0 or x > 168:
+#     print("INVALID")
+# elif x <= 40:
+#     total_amount = x * 8
+#     print("YOU MADE", total_amount, "DOLLARS THIS WEEK")
+# elif x <= 50:
+#     total_amount = (40 * 8) + ((x - 40) * 9)
+#     print("YOU MADE", total_amount, "DOLLARS THIS WEEK")
+# else: 
+#     total_amount = (40 * 8) + (10*9)+((x-50) * 10)
+#     print("YOU MADE", total_amount, "DOLLARS THIS WEEK")
