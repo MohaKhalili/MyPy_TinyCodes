d2227c1376dbb3be7a65ed1202d676313a5539fe
mohakhalili71 <md.kh71@gmail.com>
2019-12-20 17:06:34 +0330

    Part 6 in exam 6 edited

--- 04 - Exam 2/Part6.py
+++ 04 - Exam 2/Part6.py
@@ -4,18 +4,34 @@
 # seconds and prints them exactly in the format specified below. 
 
 # Type your code here
-n = input("please enter an time (second): ")
-n = int(n)
-total_amount = 24*60*60
-if n >= total_amount:
-    day = n // total_amount
-    hour = (n - day * total_amount) // 3600
-    minu = (n - day * total_amount - hour * 3600) // 60
-    second = (n - day * total_amount - hour * 3600 - minu * 60)
-    print(day,"days",hour,"hours",minu,"minutes",second,"seconds")
-else:
-    day = n // total_amount
-    hour = n // 3600
-    minu = (n - hour * 3600) // 60
-    second = (n - hour * 3600 - minu * 60)
-    print(day,"days",hour,"hours",minu,"minutes",second,"seconds")
+all_second = input("please enter a number:")
+all_second = int(all_second)
+
+s_days = 24*3600                                            # all second we have in a day
+days_cal = all_second // s_days                             # Calculate the days
+all_hour_Tosecond = all_second - (days_cal * s_days)        # remaining seconds after we get days
+hour_cal = all_hour_Tosecond // (60 * 60)                   # Calculate the hours
+all_minu_Tosecond = all_hour_Tosecond - (hour_cal * 3600)   # remaining seconds after we get hours
+minu_cal = all_minu_Tosecond // 60                          # get the minutes
+second_cal = all_minu_Tosecond - (minu_cal * 60)            # Finally calculate the remaining seconds after we get the minutes
+print(days_cal,"days",hour_cal,"hours",minu_cal,"minutes",second_cal,"seconds")
+
+
+################### Sample Solution ###################
+# user_response = input("Enter a positive integer (seconds):")
+# given_seconds = int(user_response)
+# Calculate the days
+# days = given_seconds//(24*60*60)
+# seconds_1 = given_seconds % (24*60*60)    # remaining seconds after we get days
+
+# Calculate the hours
+# hours = seconds_1//(60*60)          # get the hours out of seconds_1
+# seconds_2 = seconds_1 % (60*60)     # remaining seconds after we get hours
+
+# Calculate the minutes
+# minutes = seconds_2 // 60           # get the minutes
+
+# Finally calculate the remaining seconds after we get the minutes
+# seconds = seconds_2 % 60
+
+# print(days, "days", hours, "hours", minutes, "minutes", seconds, "seconds")
