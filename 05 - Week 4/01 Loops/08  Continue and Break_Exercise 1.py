4d926f7d3176fc9961076586695133ca3b8b1f3c
mohakhalili71 <md.kh71@gmail.com>
2019-12-21 16:34:26 +0330

    Break & continue EXs edited

--- 05 - Week 4/01 Loops/08  Continue and Break_Exercise 1.py
+++ 05 - Week 4/01 Loops/08  Continue and Break_Exercise 1.py
@@ -1,1 +1,27 @@
-# What will be printed after each of the following code segments? If error, then write Error
+# What will be printed after each of the following code segments? 
+# If error, then write Error.
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+count = 20
+for x in range(0,10):
+    count = count - 1
+    if x == 2:
+        break
+print(count)
+###################################################################
+k = 1
+while k<5:
+    if k % 7 == 0:
+        break
+    k = k + 2
+print(k)
+###################################################################
+my_list = ["dog", 24, "cat", 12]
+count = 0
+for element in my_list:
+    if element == "cat":
+        continue
+    count = count + 1
+print(count)
--- 05 - Week 4/01 Loops/09  Continue and Break_Exercise 2.py
+++ 05 - Week 4/01 Loops/09  Continue and Break_Exercise 2.py
@@ -1,1 +1,31 @@
-# What will be printed after each of the following code segments? If error, then write Error
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+my_str = "university"
+count = 0
+for char in my_str:
+    if char == "i":
+        continue
+    else:
+        count = count + 1
+print(count)
+###################################################################
+my_str = "university"
+count = 0
+for char in my_str:
+    count = count + 1
+    if char == "e":
+        break  
+print(count)
+###################################################################
+my_list = [6, 5, 7, 2, 3, 5, 7, 8] 
+count = 0
+for number in my_list:
+    if number == 5 or number == 7:
+        continue
+    else:
+        count = count + number
+print(count)
