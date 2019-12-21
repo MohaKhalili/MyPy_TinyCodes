a25686391e01a1506ba004752b02703cf6bd20a9
mohakhalili71 <md.kh71@gmail.com>
2019-12-21 14:36:52 +0330

    The sum of every third numbers from 1 to 1001 edited

--- 05 - Week 4/01 Loops/03 While Loop_Exercise 3.py
+++ 05 - Week 4/01 Loops/03 While Loop_Exercise 3.py
@@ -1,4 +1,4 @@
-# a program using while loop, which prints the sum of every third 
+# A program using while loop, which prints the sum of every third 
 # numbers from 1 to 1001 ( both 1 and 1001 are included) 
 
 # Type your code here
@@ -10,3 +10,11 @@ while (number >= 1):
         number_sum = number_sum + number
     number = number - 1
 print(number_sum)
+
+################### Sample Solution ###################
+# x = 1
+# count = 0
+# while x < 1001:
+#     count = count +x
+#     x = x + 3
+# print(count)
