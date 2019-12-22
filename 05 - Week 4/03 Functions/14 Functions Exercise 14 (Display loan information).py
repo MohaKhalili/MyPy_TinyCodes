3780692f489558ee252c6183f3569b542be7f225
mohakhalili71 <md.kh71@gmail.com>
2019-12-22 11:44:16 +0330

    Ex 14 for function edited

--- 05 - Week 4/03 Functions/14 Functions Exercise 14 (Display loan information).py
+++ 05 - Week 4/03 Functions/14 Functions Exercise 14 (Display loan information).py
@@ -36,37 +36,75 @@
 #  * Use floating point numbers in your calculations.
 #  * Convert all the numbers to int only for printing them. 
 
-# Your function for calculating payment goes here
-def Mounthly_payment(principal, annual_interest_rate, duration):
-    n = duration * 12
-    if annual_interest_rate != 0:
-        r = (annual_interest_rate / 100) / 12
-        Mounthly_payment = (principal * (r * ((1+r)**n))) / (((1+r)**n) - 1)
-    else:
-        Mounthly_payment = principal / n
-    return Mounthly_payment
+# # Your function for calculating payment goes here
+# def Mounthly_payment(principal, annual_interest_rate, duration):
+#     n = duration * 12
+#     if annual_interest_rate != 0:
+#         r = (annual_interest_rate / 100) / 12
+#         Mounthly_payment = (principal * (r * ((1+r)**n))) / (((1+r)**n) - 1)
+#     else:
+#         Mounthly_payment = principal / n
+#     return Mounthly_payment
 
 
-# Your function for calculating remaining balance goes here
-def Remaining_loan_balance(principal,annual_interest_rate,duration,number_of_payments):
-    n = duration * 12
-    p = number_of_payments
-    if annual_interest_rate != 0:
-        r = (annual_interest_rate / 100) / 12
-        Remaining_loan_balance = (principal * ( ((1+r)**n) - ((1+r)**p) )) / (((1+r)**n) - 1)
-    else:
-        Remaining_loan_balance = principal * (1- (p/n))
-    return Remaining_loan_balance         
+# # Your function for calculating remaining balance goes here
+# def Remaining_loan_balance(principal,annual_interest_rate,duration,number_of_payments):
+#     n = duration * 12
+#     p = number_of_payments
+#     if annual_interest_rate != 0:
+#         r = (annual_interest_rate / 100) / 12
+#         Remaining_loan_balance = (principal * ( ((1+r)**n) - ((1+r)**p) )) / (((1+r)**n) - 1)
+#     else:
+#         Remaining_loan_balance = principal * (1- (p/n))
+#     return Remaining_loan_balance         
 
-# Your main program goes here
-principal=float(input("Enter loan amount: "))
-annual_interest_rate=float(input("Enter annual interest rate (percent): "))
-duration=int(input("Enter loan duration in years: "))
+# # Your main program goes here
+# principal=float(input("Enter loan amount: "))
+# annual_interest_rate=float(input("Enter annual interest rate (percent): "))
+# duration=int(input("Enter loan duration in years: "))
 
-print("LOAN AMOUNT:",int(principal),"INTEREST RATE (PERCENT):",int(annual_interest_rate))
-print("DURATION (YEARS):",duration,"MONTHLY PAYMENT:",int(Mounthly_payment(principal,annual_interest_rate,duration)))
+# print("LOAN AMOUNT:",int(principal),"INTEREST RATE (PERCENT):",int(annual_interest_rate))
+# print("DURATION (YEARS):",duration,"MONTHLY PAYMENT:",int(Mounthly_payment(principal,annual_interest_rate,duration)))
 
-for i in range(1,duration+1):
-    BALANCE = Remaining_loan_balance(principal,annual_interest_rate,duration,i*12)
-    TOTAL_PAYMENT = i*12*Mounthly_payment(principal,annual_interest_rate,duration)
-    print("YEAR:",i,"BALANCE:",int(BALANCE),"TOTAL PAYMENT",int(TOTAL_PAYMENT))
+# for i in range(1,duration+1):
+#     BALANCE = Remaining_loan_balance(principal,annual_interest_rate,duration,i*12)
+#     TOTAL_PAYMENT = i*12*Mounthly_payment(principal,annual_interest_rate,duration)
+#     print("YEAR:",i,"BALANCE:",int(BALANCE),"TOTAL PAYMENT",int(TOTAL_PAYMENT))
+
+
+def Loan_Information(loan_amount, annual_interest_rate, duration):
+
+    def MP_loan_calc(principal, annual_interest_rate, duration):
+        n = duration * 12
+        if annual_interest_rate != 0:
+            r = (annual_interest_rate / 100) / 12
+            Mounthly_payment = (principal * (r * ((1+r)**n))) / (((1+r)**n) - 1)
+        else:
+            Mounthly_payment = principal / n
+        return Mounthly_payment
+
+    def Remaining_loan_balance(principal,annual_interest_rate,duration,number_of_payments):
+        n = duration * 12
+        p = number_of_payments
+        if annual_interest_rate != 0:
+            r = (annual_interest_rate / 100) / 12
+            Remaining_loan_balance = (principal * ( ((1+r)**n) - ((1+r)**p) )) / (((1+r)**n) - 1)
+        else:
+            Remaining_loan_balance = principal * (1- (p/n))
+        return Remaining_loan_balance
+
+    result_MP_loan_calc = MP_loan_calc(loan_amount, annual_interest_rate, duration)
+    
+    print ("LOAN AMOUNT:",int(loan_amount),"INTEREST RATE (PERCENT):",int(annual_interest_rate))
+    print("DURATION (YEARS):",int(duration),"MONTHLY PAYMENT:",int(result_MP_loan_calc))
+
+    for i in range(1,duration+1):
+        result_Remaining_loan_balance = Remaining_loan_balance(loan_amount,annual_interest_rate,duration,i*12)
+        TOTAL_PAYMENT = result_MP_loan_calc * i * 12
+        print("YEAR:",i,"BALANCE:", int(result_Remaining_loan_balance),"TOTAL PAYMENT", int(TOTAL_PAYMENT))
+
+# Driver code test
+principal = float(input("Please enter the all of loan value : "))
+annual_interest_rate = float(input("Please enter the annual interest rate : "))
+duration = int(input("Please enter the loan payment duration : "))
+Loan_Information(principal, annual_interest_rate, duration)
