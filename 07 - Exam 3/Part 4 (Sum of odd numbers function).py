7983b01c4e95de6e8f3f20f204fddce54b366200
mohakhalili <md.kh71@gmail.com>
2019-12-23 10:31:04 +0330

    Exam 3, Part 4 method 2 and solution sample added

--- 07 - Exam 3/Part 4 (Sum of odd numbers function).py
+++ 07 - Exam 3/Part 4 (Sum of odd numbers function).py
@@ -9,10 +9,43 @@
 # in your code. So, if you want to write more than one function to help you solve the problem, 
 # remember to embed the helper functions inside the first function. 
 
-# Type your code here
+# method 1
 def odd_number(n):
     sum_n = 0
     for i in n:
         if i % 2 != 0:
             sum_n = sum_n + i
     return sum_n
+
+# method 2
+def sum_odd_number(list):
+    sum_number = 0
+    for i in range(len(list)):
+        if list[i] % 2 == 1:
+            sum_number = sum_number + list[i]
+    return sum_number 
+
+def list_create():
+    lst = [] 
+    # number of elemetns as input 
+    n = int(input("Enter number of elements : ")) 
+  
+    # iterating till the range 
+    for i in range(0, n): 
+        ele = int(input("enter list element : ")) 
+  
+        lst.append(ele) # adding the element
+    return lst 
+
+# Driver code test
+my_list = list_create()
+result = sum_odd_number(my_list)
+print(result)
+
+################### Sample Solution ###################
+# def _sum_of_odds_sampleX_(my_list):
+#     my_sum = 0
+#     for items in my_list:
+#         if items % 2 != 0:
+#             my_sum = my_sum + items
+#     return my_sum
