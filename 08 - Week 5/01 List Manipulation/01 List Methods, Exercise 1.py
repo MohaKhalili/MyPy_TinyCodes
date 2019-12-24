8b8f6ebc7d89defb52e91770e749cff6342d4283
mohakhalili <md.kh71@gmail.com>
2019-12-24 19:29:17 +0330

    list method, Ex1 added

---
+++ 08 - Week 5/01 List Manipulation/01 List Methods, Exercise 1.py
@@ -0,0 +1,21 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+MyList = [3, "dog", 9, "caat"]
+print(len(MyList))
+
+##################################################################
+my_list = ['two', 5, ['one', 2]]
+print(len(my_list))
+
+##################################################################
+MyList = ['pet', 'dog', 5, 'cat', 'good', 'dog']
+print(MyList.count('dog'))
+
+##################################################################
+MyList = ['pet' , 'dog' ,5, 'cat', 'good','dog']
+x = MyList.count('car')
+print(x)
