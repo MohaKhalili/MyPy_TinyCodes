44d840497864c70ca45bb25da68cf760012dbc40
mohakhalili <md.kh71@gmail.com>
2019-12-24 19:33:53 +0330

    list method, Ex2 added

---
+++ 08 - Week 5/01 List Manipulation/02 List Methods, Exercise 2.py
@@ -0,0 +1,25 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+my_list = [4, 'two' , 'one' ,5,3,2,'bye']
+print (my_list.index('one'))
+
+###################################################################
+MyList = ['pet' , 'dog' ,5, 'cat', 'good','dog']
+print (MyList.index(5))
+
+###################################################################
+MyList = ['pet' , 'dog' ,5, 'cat', 'good','dog']
+print (MyList.index('dog'))
+  
+###################################################################
+MyList = ['pet' , 'dog' ,5, 'cat', 'good','dog']
+print (MyList.index('car'))
+  
+###################################################################
+MyList = [1, 5, 67, 3, 4]
+x = MyList.sort()
+print (x)
