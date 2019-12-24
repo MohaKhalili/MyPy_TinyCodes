b1d89cd240e07e2b1b53b1be3412dca7e8cac39c
mohakhalili <md.kh71@gmail.com>
2019-12-24 19:42:27 +0330

    list method, Ex3 added

---
+++ 08 - Week 5/01 List Manipulation/03 List Methods, Exercise 3.py
@@ -0,0 +1,31 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+MyList = [3, 7, 9, 0, 4]
+MyList.sort()
+print (MyList)
+
+#################################################################
+MyList = ['cow', 'cat', 'dog', 'pet']
+MyList.sort()
+print (MyList)
+
+#################################################################
+MyList = [3, "dog", 9, "cat"]
+MyList.extend([7,8])
+print (MyList)
+  
+#################################################################
+MyList = ['dog',3]
+MyList.extend('cat')
+print (MyList)
+  
+#################################################################
+MyList = ['pet' , 'dog' ,5]
+MyList.append('cat')
+print(MyList)
+
+#################################################################
+my_list = ['two' , 'one' ,5,3,2,'bye']
+my_list.append (['one',2])
+print(my_list)
