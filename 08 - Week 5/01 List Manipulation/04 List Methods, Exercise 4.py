d2e89aaade9add034996f3c490ee4c8f6520b99b
mohakhalili <md.kh71@gmail.com>
2019-12-24 19:46:10 +0330

    list method, Ex4 added

---
+++ 08 - Week 5/01 List Manipulation/04 List Methods, Exercise 4.py
@@ -0,0 +1,26 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+my_list=[1,'dog',2.3]
+my_list.insert(2,'cat')
+print(my_list)
+  
+####################################################################
+MyList = [3, "dog", 9, "cat"]
+MyList.insert(1, [6, 9])
+print (MyList)
+  
+####################################################################
+MyList = [3, "dog", 9, "cat"]
+MyList.remove(3)
+print (MyList)
+  
+####################################################################
+MyList = [3, "dog", 9, "cat"]
+MyList.pop()
+print (MyList)
+
+####################################################################
+MyList = [3, "dog", 0, "cat"]
+del MyList[0]
+print (MyList)
