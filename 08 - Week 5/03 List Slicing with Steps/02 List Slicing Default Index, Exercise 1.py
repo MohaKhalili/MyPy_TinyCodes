45a68bcfd96a75df46b94b1f64de10be40bbb8a1
mohakhalili <md.kh71@gmail.com>
2019-12-25 19:11:25 +0330

    List Slicing Default Index, Exercise 1 added

---
+++ 08 - Week 5/03 List Slicing with Steps/02 List Slicing Default Index, Exercise 1.py
@@ -0,0 +1,28 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+my_list=[24, 'car', 'tv', 5.3, 'apple', 36]
+print(my_list[:3])
+
+###################################################################
+my_list=[24 ,'car', 'tv', 5.3, 'apple', 36]
+print(my_list[2:])
+
+###################################################################
+my_list=[24, 'car', 'tv', 5.3, 'apple', 36]
+print(my_list[1:3:])
+
+###################################################################
+my_list=[24, 'car', 'tv', 5.3, 'apple', 36]
+print(my_list[2::3])
+
+###################################################################
+my_list=[24, 'car', 'tv', 5.3, 'apple', 36]
+print(my_list[::2])
+
+###################################################################
+my_list=[24, 'car', 'tv', 5.3, 'apple', 36]
+print(my_list[::])
