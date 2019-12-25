330e0b821fb4e39a11462f31025d6be97fb4156f
mohakhalili <md.kh71@gmail.com>
2019-12-25 11:49:26 +0330

    List Slicing Negative Index, Exercise 1.py

---
+++ 08 - Week 5/02 List slicing/04  List Slicing Negative Index, Exercise 1.py
@@ -0,0 +1,17 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+my_list=[54,'cow',0.25,32,'worm']
+print(my_list[0:-2])
+
+#################################################################
+my_list=[54,'cow',0.25,32,'worm']
+print(my_list[2:-2])
+
+#################################################################
+my_list=[54,'cow',0.25,32,'worm']
+print(my_list[3:-3])
+
+#################################################################
+my_list=[54,'cow',0.25,32,'worm']
+print(my_list[0:-1])
