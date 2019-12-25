9b515a13c1f760c9e9dad4f8094758b528adbdbc
mohakhalili <md.kh71@gmail.com>
2019-12-25 19:07:06 +0330

    List Slicing With Step, Exercise 1 added

---
+++ 08 - Week 5/03 List Slicing with Steps/01 List Slicing With Step, Exercise 1.py
@@ -0,0 +1,20 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+my_list = ['python', 3.4, 54, 'java', 82, 'c', 7.4]
+print(my_list[0:6:2])
+
+###################################################################
+my_list = ['python', 3.4, 54, 'java', 82, 'c', 7.4]
+print(my_list[6:0:-2])
+
+###################################################################
+my_list=['python', 3.4, 54, 'java', 82, 'c', 7.4]
+print(my_list[0:-1:3])
+
+###################################################################
+my_list=['python', 3.4, 54, 'java', 82, 'c', 7.4]
+print(my_list[-1:0:-1])
