c9fa5e4482bb0e43839399d3056e9c10f2e8c8b8
mohakhalili <md.kh71@gmail.com>
2019-12-25 11:23:48 +0330

    List Slicing, Exercise 1 added

---
+++ 08 - Week 5/02 List slicing/01 List Slicing, Exercise 1.py
@@ -0,0 +1,17 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+my_list=['pet', 12, 'cat', 4.3, 'dog', 46]
+print (my_list[0:1])
+
+#################################################################
+my_list=['pet', 12, 'cat', 4.3, 'dog', 46]
+print(my_list[1:3])
+
+#################################################################
+my_list=['pet', 12, 'cat', 4.3, 'dog', 46]
+print(my_list[0:4])
+
+#################################################################
+my_list=['pet', 12, 'cat', 4.3, 'dog', 46]
+print(my_list[0:6])
