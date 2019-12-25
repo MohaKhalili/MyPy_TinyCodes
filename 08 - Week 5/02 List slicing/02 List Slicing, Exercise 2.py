1c195ba3a0d4aa1703d3b445e813396354b34dc2
mohakhalili <md.kh71@gmail.com>
2019-12-25 11:35:38 +0330

    List Slicing, Exercise 2 added

---
+++ 08 - Week 5/02 List slicing/02 List Slicing, Exercise 2.py
@@ -0,0 +1,21 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+my_list = [5, 'old', 'new', 8, 'time', 2]
+print(my_list[2:4])
+
+#################################################################
+my_list = [5, 'old', 'new', 8, 'time', 2]
+print(my_list[0:3])
+
+#################################################################
+my_list = [5, 'old', 'new', 8, 'time', 2]
+print(my_list[3:2])
+
+#################################################################
+my_list = [5, 'old', 'new', 8, 'time', 2]
+print(my_list[3:7])
+
+#################################################################
+my_list = [5, 'old', 'new', 8, 'time', 2]
+print(my_list[0:1])
