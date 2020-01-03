8f29c313424a97f69385b60e7942410bb1c7ab4b
mohakhalili <md.kh71@gmail.com>
2020-01-03 17:17:08 +0330

    Strings, Exercise 2 added

---
+++ 10 - Week 6/01 Strings/02 Strings, Exercise 2.py
@@ -0,0 +1,17 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match 
+# the correct Python output.
+
+x = "abcdefg"
+print (x[-5:4])
+#################################################################
+x = "abcdefg"
+print (x[5:1:-2]) 
+#################################################################
+x = "abcdefg"
+print (x[-6:len(x):2]) 
+#################################################################
+x = "abcdefg"
+print (x[-4:-1:2]) 
