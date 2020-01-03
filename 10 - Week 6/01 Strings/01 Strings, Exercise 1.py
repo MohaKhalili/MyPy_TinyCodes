e020a2ce470c3db5f03041555c39e413b079a259
mohakhalili <md.kh71@gmail.com>
2020-01-03 17:14:50 +0330

    Strings, Exercise 1 added

---
+++ 10 - Week 6/01 Strings/01 Strings, Exercise 1.py
@@ -0,0 +1,17 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match 
+# the correct Python output.
+
+x="university" 
+print (x[0:4])
+################################################################
+x="university" 
+print (x[-5:-3])
+################################################################
+x="university" 
+print (x[-5:-2])
+################################################################
+x="university" 
+print (x[5:0:-2])
