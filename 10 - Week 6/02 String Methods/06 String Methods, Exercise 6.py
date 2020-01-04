fb5bcc430f8a9c79f388ca1ed8f3b8960c172c06
mohakhalili <md.kh71@gmail.com>
2020-01-04 11:46:34 +0330

    String Methods, Exercise 6 added

---
+++ 10 - Week 6/02 String Methods/06 String Methods, Exercise 6.py
@@ -0,0 +1,20 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error.
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+x="hello are you there"
+print (x.split())
+#########################################################################
+my_str = "hello hello"
+print (my_str.split('l'))
+#########################################################################
+my_str='Computer science'
+print (my_str.split ('e'))
+#########################################################################
+x="frequency of letters"
+print (x.split("e",2))
+#########################################################################
+x="Mississippi"
+print (x.split("s",3))
