055aad9f62654f21f53400d1b712420d85303386
mohakhalili <md.kh71@gmail.com>
2020-01-04 11:10:05 +0330

    String Methods, Exercise 1 added

---
+++ 10 - Week 6/02 String Methods/01 String Methods, Exercise 1.py
@@ -0,0 +1,23 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+my_str = "Hello"
+print (my_str.islower())
+#####################################################################
+my_str = "Goodbye"
+print (my_str.lower())  
+#####################################################################
+my_str = "GoodBye"
+print (my_str.isupper())
+#####################################################################
+my_str = "Hello"
+print (my_str.upper())
+#####################################################################
+my_str = "GoodBye"
+print (my_str.swapcase())
+#####################################################################
+my_str = "computer science engineering"
+print (my_str.title())
