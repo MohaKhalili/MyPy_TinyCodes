3381ea42336851183ab057ccf98c4714788a897e
mohakhalili <md.kh71@gmail.com>
2020-01-04 11:43:15 +0330

    String Methods, Exercise 5 added

---
+++ 10 - Week 6/02 String Methods/05 String Methods, Exercise 5.py
@@ -0,0 +1,22 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+x="oops too"
+print (x.strip("o")) 
+#####################################################################
+my_str = "oops too"
+print (my_str.lstrip('o'))
+#####################################################################
+my_str = "oops too"
+print (my_str.rstrip('o'))
+#####################################################################
+my_str = "hello hello"
+print (my_str.rstrip('h'))
+#####################################################################
+print ("NO".join("test")) 
+#####################################################################
+x= "test".join(["A","B","C"])
+print (x) 
