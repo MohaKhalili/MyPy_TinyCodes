244f7ac8e7a903b137b9147aa8d8aa47b4f39e0b
mohakhalili <md.kh71@gmail.com>
2020-01-04 11:12:38 +0330

    String Methods, Exercise 2 added

---
+++ 10 - Week 6/02 String Methods/02 String Methods, Exercise 2.py
@@ -0,0 +1,21 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+print ("hello".isalpha())
+####################################################################
+my_str = "hello there"
+print (my_str.isalpha())
+####################################################################
+my_str = "hello12"
+print (my_str.isalpha())
+####################################################################
+my_str = "CSE1309"
+print (my_str.isalnum())
+####################################################################
+my_str = "CSE-1309"
+print (my_str.isalnum())
+####################################################################
+print ("bird".endswith("ir"))  
