72754c384751011ab630718353c02555c2cab1bb
mohakhalili <md.kh71@gmail.com>
2020-01-04 11:39:48 +0330

    String Methods, Exercise 4 added

---
+++ 10 - Week 6/02 String Methods/04 String Methods, Exercise 4.py
@@ -0,0 +1,21 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+print ("university".index("iv"))
+###################################################################
+my_str = "this is a test"
+print (my_str.count('s',5)) 
+###################################################################
+print ('test'.center(10,'x'))
+###################################################################
+my_str = "Hello"
+print (my_str.center(9,'z'))
+###################################################################
+my_str = "CSE"
+print (my_str.ljust(7,'x'))
+###################################################################
+my_string = 'hello'
+print (my_string.rjust(8,"A"))
