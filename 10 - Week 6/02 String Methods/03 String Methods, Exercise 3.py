1ab242cf86243544542f28efdc49523675094e52
mohakhalili <md.kh71@gmail.com>
2020-01-04 11:18:10 +0330

    String Methods, Exercise 3 added

---
+++ 10 - Week 6/02 String Methods/03 String Methods, Exercise 3.py
@@ -0,0 +1,18 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+print ("abcdef".find("e"))
+###################################################################
+x="Mississippi"
+print (x.rfind('si',3)) 
+###################################################################
+my_string = "knickknack"
+print (my_string.rfind('k',3,-2))
+###################################################################
+my_str='Engineering'
+print (my_str.replace('e','x'))  
+###################################################################
+print ("Mississippi".replace('i','z',2))
