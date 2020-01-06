ba5ddb5f4beebe23cea362386784120b11fd231b
mohakhalili <md.kh71@gmail.com>
2020-01-06 22:43:11 +0330

    Find mismatch function edited

--- 11 - Assignment 2/01_find_mismatch.py
+++ 11 - Assignment 2/01_find_mismatch.py
@@ -1,29 +1,27 @@
-# a function named find_mismatch that accepts
-# two strings as input arguments and returns:
+# A function named find_mismatch that accepts two strings as input arguments and returns:
 #       0   if the two strings match exactly.
 #       1   if the two strings have the same length
 #           and mismatch in only one character.
 #       2   if the two strings do not have the same
 #           length or mismatch in two or more characters.
-from unittest import result
 
 
-def find_mismatch(s1,s2):
-    s1 = s1.lower()
-    s2 = s2.lower()
-    if len(s1) != len(s2):
-        result = 2
-    elif len(s1) == len(s2):
-        count = 0
-        for index in range(len(s1)):
-            if s1[index] != s2[index]:
-                count = count + 1
-        if count > 1:
-            result = 2
-        elif count == 1:
+def find_mismatch(string1, string2):
+    string1 = string1.lower()
+    string2 = string2.lower()
+    if string1 == string2:
+        result = 0
+    elif len(string1) == len(string2):
+        counter = 0
+        for i in range(len(string1)):
+            if string1[i] != string2[i]:
+                counter += 1
+        if counter == 1:
             result = 1
         else:
-            result = 0
+            result = 2
+    else:
+        result = 2
     return result
 
 # driver code test
