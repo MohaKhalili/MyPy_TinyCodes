b98f415a85a21888776d7f252262532b465526f6
mohakhalili <md.kh71@gmail.com>
2020-01-18 11:50:11 +0330

    Spelling_corrector function added

--- 11 - Assignment 2/03_spelling_corrector.py
+++ 11 - Assignment 2/03_spelling_corrector.py
@@ -26,85 +26,170 @@
 #     Assume that the input string only includes alphabetic characters and spaces. (a-z and A-Z)
 #     Remove extra spaces between the words.
 #     Remove spaces at the start and end of the output string.
-def spelling_corrector(s1,s2):
-    new_string = ""
-    correct_spells = s2
-    string_splited = s1.split()
 
-# Definition required functions
-    def find_mismatch(s1, s2):
-        s1 = s1.lower()
-        s2 = s2.lower()
-        if len(s1) != len(s2):
-            result = 2
-        elif len(s1) == len(s2):
-            count = 0
-            for index in range(len(s1)):
-                if s1[index] != s2[index]:
-                    count = count + 1
-            if count > 1:
-                result = 2
-            elif count == 1:
+def Spelling_corrector(input_string, input_list):
+
+    input_string = input_string.split()
+    output_string = ''
+
+    def single_insert_or_delete(string1, string2):
+        string1 = string1.lower()
+        string2 = string2.lower()
+        len_string1 = len(string1)
+        len_string2 = len(string2)
+        if string1 == string2:
+            return 0
+    
+        if abs(len_string1-len_string2)!=1:
+            return 2
+
+        if max(len_string1, len_string2) == min(len_string1, len_string2)+1:
+            if len_string1 > len_string2:
+                maximum_string = string1
+                minimum_string = string2
+            else:
+                maximum_string = string2
+                minimum_string = string1
+            test_string = ''
+            for i in range(len(maximum_string)):
+                test_string = maximum_string.replace(maximum_string[i],'',1)
+                if test_string == minimum_string:
+                    return 1
+                    break
+                test_string = ''
+            return 2
+
+    def find_mismatch(string1, string2):
+        string1 = string1.lower()
+        string2 = string2.lower()
+        if string1 == string2:
+            result = 0
+        elif len(string1) == len(string2):
+            counter = 0
+            for i in range(len(string1)):
+                if string1[i] != string2[i]:
+                    counter += 1
+            if counter == 1:
                 result = 1
             else:
-                result = 0
-        return result
-
-    def single_insert_or_delete(s1, s2):
-        s1 = s1.lower()
-        s2 = s2.lower()
-        result = 0
-        if len(s1) >= len(s2):
-            max_string = s1
-            min_string = s2
+                result = 2
         else:
-            max_string = s2
-            min_string = s1
-        if s1 != s2:
             result = 2
-            if len(s1) != len(s2):
-                if (max_string.replace(max_string[-1], "", 1) == min_string) or (
-                        max_string.replace(max_string[0], "", 1) == min_string):
-                    result = 1
-                for index in range(min(len(s1), len(s2))):
-                    if s1[index] != s2[index]:
-                        max_string = max_string.replace(max_string[index], "", 1)
-                        if max_string == min_string:
-                            result = 1
-                            break
-                        max_string = max(s1, s2)
         return result
 
-    for word in string_splited:
-        if len(word) > 2:
-            count = 0
-            for word_correct_spells in correct_spells:
-                if single_insert_or_delete(word_correct_spells, word) == 0:
-                    new_string = new_string + word.lower() + " "
-                    count = 1
-                    break
-                elif (single_insert_or_delete(word_correct_spells, word) == 1):
-                    new_string = new_string + word_correct_spells.lower() + " "
-                    count = 1
-                    break
-                elif find_mismatch(word_correct_spells, word) == 1:
-                    new_string = new_string + word_correct_spells.lower() + " "
-                    count = 1
-                    break
-            if count == 0:
-                new_string = new_string + word.lower() + " "
-        else:
-            new_string = new_string + word.lower() + " "
-    result = new_string.strip()
-    return result
+    for word in input_string:
+
+        # Not checking one or two letter words (copy them directly to the output string).
+        if len(word) <= 2:
+            output_string += word
+            if word != input_string[-1]:
+                    output_string += " "
+            continue
+        
+        # Then we check all the words in the list to find exactly the same word
+        for item in input_list:
+
+            result_find_mismatch = find_mismatch(word, item)                        # Calling find_mismatch function for the word of string and the word of list
+            result_single_insert_or_delete = single_insert_or_delete(word , item)   # Calling single_insert_or_delete function for the word of string and the word of list
+            counter = 0                                                             # initial value for flag of changes
+            
+            if result_find_mismatch == 0:               # if two words is the same
+                output_string += word
+                counter += 1                            # The flag is up, if the Change has occurred in this condition
+                if word != input_string[-1]:            # added a space character, if the word is not the last one.
+                    output_string += " "
+                break  
+
+        # If there are no words in the list that match the string word, so check words spelled for other cases
+        if counter != 1:
+            for item in input_list:
+
+                result_find_mismatch = find_mismatch(word, item)                        # Calling find_mismatch function for the word of string and the word of list
+                result_single_insert_or_delete = single_insert_or_delete(word , item)   # Calling single_insert_or_delete function for the word of string and the word of list
+                counter = 0                                                             # initial value for flag of changes
+
+                # if the two strings have the same length and mismatch in only one character.
+                # or if the first string can become the same as the second string by inserting or deleting a single character. 
+                if result_find_mismatch == 1 or result_single_insert_or_delete == 1:    
+                    output_string += item
+                    counter += 1                            # The flag is up, if the Change has occurred in this condition
+                    if word != input_string[-1]:            # added a space character, if the word is not the last one.
+                        output_string += " "
+                    break                                   # break from this loop for choosen item
+                
+                # if the two strings have the same length and mismatch in only one character.
+                # and if the two strings can become the same by Replacing one character
+                elif result_find_mismatch == 1 and result_single_insert_or_delete == 2:
+                    output_string += item
+                    counter += 1                            # The flag is up, if the Change has occurred in this condition
+                    if word != input_string[-1]:            # added a space character, if the word is not the last one.
+                        output_string += " "
+                    break                                   # break from this loop for choosen item
+
+        # If none of the above conditions apply, move the word from the input string to the output string
+        if counter == 0:
+            output_string += word
+            if word != input_string[-1]:            # added a space character, if the word is not the last one.
+                output_string += " "
+    return output_string
 
 
 # driver code tester
-
-#String = "Asignment three xample inpu "
-#correct_spells = ['Assignmen', 'tree', 'three', 'example', 'output', 'input']
-
-String = "Thes is the Firs cas"
-correct_spells = ['that','first','case','car']
-result = spelling_corrector(String, correct_spells)
+String = "Asignment three xample inpu"
+correct_spells = ['Assignmen', 'tree', 'three', 'example', 'output', 'input']
+result = Spelling_corrector(String, correct_spells)
 print(result)
+
+################### Instructor function ###################
+# def spelling_corrector (s,correct_spelled):
+#     words=s.strip().split()
+#     output_str=""
+#     for current_word in words:
+#         if len(current_word)<=2 or (current_word in correct_spelled) :
+#             output_str=output_str+" "+current_word
+#             continue
+#         min_mismatch=2
+#         replacement_word=current_word
+#         for correct_word in correct_spelled:
+#             if min(_find_mismatch(current_word,correct_word), _single_insert_or_delete(current_word,correct_word))==1:
+#                 replacement_word=correct_word
+#                 break
+#         output_str=output_str+" "+replacement_word
+#     return output_str.strip().lower()
+# def _find_mismatch(s1,s2):
+#     if len(s1) != len(s2):
+#         return 2
+#     s1=s1.lower()
+#     s2=s2.lower()
+#     number_of_mismatches=0
+#     for index in range(len(s1)):
+#         if s1[index] != s2[index]:
+#             number_of_mismatches=number_of_mismatches+1
+#             if number_of_mismatches>1:
+#                 return 2
+#     return number_of_mismatches 
+# def _single_insert_or_delete(s1,s2):
+#     s1=s1.lower()
+#     s2=s2.lower()
+#     if s1==s2:
+#         return 0
+#     if abs(len(s1)-len(s2))!=1:
+#         return 2
+
+#     if len(s1)>len(s2):
+#         # only deletion is possible
+#         for k in range(len(s2)):
+#             if s1[k]!=s2[k]:
+#                 if s1[k+1:]==s2[k:]:
+#                     return 1
+#                 else:
+#                     return 2
+#         return 1
+#     else: # s1 is shorter Only insertion is possible
+#         for k in range(len(s1)):
+#             if s1[k]!=s2[k]:
+#                 if s1[k:]==s2[k+1:]:
+#                     return 1
+#                 else:
+#                     return 2
+#         return 1
