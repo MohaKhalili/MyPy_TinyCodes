df420ddbf3aa9a5e4fc7f8b29d50f1ac107f00b3
mohakhalili <md.kh71@gmail.com>
2020-01-20 18:53:32 +0330

    initial commit

---
+++ 13 - Week 7/01 Different IDE/WHY.py
@@ -0,0 +1,29 @@
+
+# Why Use Another IDE?
+
+# It is true that you can use any plain and simple text editor for writing Python code, however, using a more powerful editor may make your life a lot easier.
+
+#     A good IDE should support functionalities such as:
+#     Code highlighting
+#     Integrated debugging
+#     Line numbering
+#     Code completion
+#     Code folding
+#     Multi tab windows
+#     Support for projects
+#     Flexible find and replace
+#     Syntax error highlighting
+#     Light weight and Fast
+#     Support for modules and libraries
+
+# Although the default IDE which is included with Python 3.x is adequate but it lacks some functionalities. It is best if you learn to use a more user friendly and more powerful IDE. There are many Python IDEs that you can download and use for free. My suggestion is to use the Wing IDE 101. This IDE is free and it is designed for  beginning programmers while having a balance between features and responsiveness.
+
+# The following unit includes a step by step guide for downloading and installing the free Wing IDE 101.
+
+# Here is a list of other free IDEs which support Python:
+
+#     Eclipse plus PyDev
+#     PyScripter
+#     Eric Python IDE
+#     PyCharm Community Edition
+#     CodeSkulptor
