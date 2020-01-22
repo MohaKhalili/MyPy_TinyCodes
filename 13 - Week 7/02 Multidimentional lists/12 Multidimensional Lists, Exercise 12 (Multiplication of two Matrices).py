ef85c8cb053f82dcde2cf92540bc8ef653418604
mohakhalili <md.kh71@gmail.com>
2020-01-22 12:47:26 +0330

    Multiplication of two Matrices function added

---
+++ 13 - Week 7/02 Multidimentional lists/12 Multidimensional Lists, Exercise 12 (Multiplication of two Matrices).py
@@ -0,0 +1,48 @@
+# A function that accepts two (matrices) 2 dimensional lists 
+# a and b of unknown lengths and returns their product. Hint: 
+# o matrices a and b can be multiplied together only if the 
+# number of columns of the first matrix(a) is the same as the 
+# ber of rows of the second matrix(b). Hint: You may import 
+# and use the numpy module but your return must be a python 
+# list not a numpy array. The input for this function will 
+# be two 2 Dimensional lists. For example if the input lists 
+# are:
+
+# a = [[2, 3, 4],
+#      [3, 4, 5]]
+# b = [[4, -3, 12],
+#      [1, 1, 5],
+#      [1, 3, 2]]
+
+# Then your function should return their product such as:
+
+# [[15, 9, 47], [21, 10, 66]] 
+
+def Multiplication_two_Matrices(Mat1, Mat2):
+    output_list = []
+    for i in range(len(Mat1)):
+        test_list = []
+        for k in range(len(Mat2[0])):
+            test_num = 0
+            for j in range(len(Mat1[i])):
+                test_num += Mat1[i][j] * Mat2[j][k]
+            test_list.append(test_num)
+        output_list.append(test_list)
+    return output_list
+
+# driver code tester
+a = [[2, 3, 4],
+     [3, 4, 5]]
+b = [[4, -3, 12],
+     [1, 1, 5],
+     [1, 3, 2]]
+
+result = Multiplication_two_Matrices(a, b)
+print(result)
+
+################### Sample Solution ###################
+# def _product_of_two_vectors_sample_(a, b):
+#     import numpy
+#     product = (numpy.mat(a) * numpy.mat(b))     # returns a numpy array
+#     product_to_list = product.tolist()          # convert the numpy array to a standard list
+#     return product_to_list
