462ba5b25370a24ae297c718c83d71085a2314e2
mohakhalili <md.kh71@gmail.com>
2020-01-23 18:43:38 +0330

    Dictionaries, Exercise 1 added

---
+++ 13 - Week 7/03 Dictionaries/01 Dictionaries, Exercise 1.py
@@ -0,0 +1,15 @@
+# What will be printed after each of the following code segments? If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the correct Python output.
+
+numbers={"one": 1, "two": 2, "three": 3}
+print (numbers["two"])
+####################################################################
+numbers={"one": "uno", "two": "dos", "three": "tres"}
+print (numbers["three"])
+####################################################################
+numbers={"one": 1, "two": 2}
+print (numbers[2])
+####################################################################
+numbers={"one": "uno", "two": "dos", "three": "tres"}
+print ("two" in numbers)
