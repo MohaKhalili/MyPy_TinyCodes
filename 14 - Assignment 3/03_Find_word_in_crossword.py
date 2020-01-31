41b13768cbf4352c36dba85734be676dd4a00065
mohakhalili <md.kh71@gmail.com>
2020-01-31 10:31:53 +0330

    Find a word in a crossword function (final ver) added

--- 14 - Assignment 3/03_Find_word_in_crossword.py
+++ 14 - Assignment 3/03_Find_word_in_crossword.py
@@ -40,39 +40,88 @@
 # Hint: You should use the functions that you developed in part 1 and part 2 as 
 # helper functions for part 3.
 
-# Type your code here
+################### My function ###################
+
+# horizontal finder
 def find_word_horizontal(crossword,word):
-    # insert the code from your function in part 1 here
     result = None
-    for index in range(len(list_2D)):
-        list_test = ''.join(list_2D[index])
-        if my_string in list_test:
+    for index in range(len(crossword)):
+        list_test = ''.join(crossword[index])
+        if word in list_test:
             row = index
-            for j in range(len(list_2D[index])):
-                if my_string[0] == list_2D[index][j]:
+            for j in range(len(crossword[index])):
+                if word[0] == crossword[index][j]:
                     col = j
                     result = [row, col]
                     break
             break
     return result
 
-
+# vertical finder
 def find_word_vertical(crossword,word):
-    # insert the code from your function in part 2 here
-    for index in range(len(list_2D)):
+    for index in range(len(crossword)):
         list_test = ''
-        for j in range(len(list_2D)):
-            list_test += list_2D[j][index]
-            if my_string in list_test:
+        for j in range(len(crossword)):
+            list_test += crossword[j][index]
+            if word in list_test:
                 col = index
-                row = list_test.index(my_string[0])
+                row = list_test.index(word[0])
                 return [row, col]
     return None
 
-
+# main function for find specified words
 def capitalize_word_in_crossword(crossword,word):
+    if find_word_horizontal(crossword, word) != None:
+        horizontal_result = find_word_horizontal(crossword, word)
+        row = horizontal_result[0]
+        col = horizontal_result[1]
+        for i in range(len(word)):
+            crossword[row][col+i] = crossword[row][col+i].upper()
+    elif find_word_vertical(crossword, word) != None:
+        vertical_result = find_word_vertical(crossword, word)
+        row = vertical_result[0]
+        col = vertical_result[1]
+        for i in range(len(word)):
+            crossword[row+i][col] = crossword[row+i][col].upper()
+    return crossword
 
 
 crossword=[['s','d','o','g'],['c','u','c','m'],['a','c','a','t'],['t','e','t','k']]
 word='cat'
-capitalize_word_in_crossword(crossword,word)
+result = capitalize_word_in_crossword(crossword,word)
+print(result)
+
+################### Instructor function ###################
+# Type your code here
+def _find_word_horizontal(crossword,word):
+    if not crossword or not word:
+        return None
+    for index,row in enumerate(crossword):
+        temp_str=''.join(row)
+        if temp_str.find(word)>=0:
+            return [index,temp_str.find(word)]
+    return None
+def _find_word_vertical(crossword,word):
+    if not crossword or not word:
+        return None    
+    number_of_columns=len(crossword[0])
+    for col_index in range (number_of_columns):
+        temp_str=''
+        for row_index in range(len(crossword)):
+            temp_str=temp_str+crossword[row_index][col_index]
+        if temp_str.find(word)>=0:
+            return [temp_str.find(word),col_index]
+    return None
+def _capitalize_word_in_crossword(crossword,word):
+    if not crossword or not word:
+        return None    
+    found_position= _find_word_horizontal(crossword,word)
+    if found_position:
+        for k in range(len(word)):
+            crossword[found_position[0]][found_position[1]+k]=crossword[found_position[0]][found_position[1]+k].upper()
+        return crossword 
+    found_position= _find_word_vertical(crossword,word)
+    if found_position:
+        for k in range(len(word)):
+            crossword[found_position[0]+k][found_position[1]]=crossword[found_position[0]+k][found_position[1]].upper()
+    return crossword
