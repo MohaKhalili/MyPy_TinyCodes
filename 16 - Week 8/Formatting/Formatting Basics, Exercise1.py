521b649ac23a7579a6a588aa7296da559ef42c40
mohakhalili <md.kh71@gmail.com>
2020-02-05 10:18:18 +0330

    Formatting Basics, Exercise1 added

---
+++ 16 - Week 8/Formatting/Formatting Basics, Exercise1.py
@@ -0,0 +1,22 @@
+
+
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match 
+# the correct Python output.
+
+my_string = "x = {} and y = {}".format (235, 5.6)
+print (my_string)
+###################################################################
+my_string = "x = {} and y = {}".format ("dog", 26)
+print (my_string)
+###################################################################
+my_string = "x = {} and y =".format ("cat", 25)
+print (my_string)
+###################################################################
+my_string = "x = {} and y = {}".format (4.3 ,"cat", 25)
+print (my_string)
+###################################################################
+my_string = "x = {} and y = {}".format ("pet")
+print (my_string)
