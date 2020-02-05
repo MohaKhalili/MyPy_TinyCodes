821a24c1958006941bec75ae60bd6af9ee495a5b
mohakhalili <md.kh71@gmail.com>
2020-02-05 10:22:41 +0330

    Formatting Positional, Exercise 1 added

---
+++ 16 - Week 8/Formatting/Formatting Positional, Exercise 1.py
@@ -0,0 +1,20 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match the 
+# correct Python output.
+
+my_string = "x = {1} and y = {0}".format ( 12 , "pet")
+print (my_string)
+#####################################################################
+my_string = "x = {2} and y = {1}".format ( "cat", 25, "dog")
+print (my_string)
+#####################################################################
+my_string = "x = {0} and y = {0}".format ( 235, 4)
+print (my_string)
+#####################################################################
+my_string = "x = {0} and y = {1}".format ([4, 'cat'], 4)
+print (my_string)
+#####################################################################
+my_string = "{1} is {0} years old".format (20 , "Jack")
+print (my_string)
