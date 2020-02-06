f04b507529cdb8bb2613663055ba5ca26220df12
mohakhalili <md.kh71@gmail.com>
2020-02-06 14:27:43 +0330

    Formatting, Exercise 6 added

---
+++ 16 - Week 8/05 Formatting Fill Align Sign/Formatting, Exercise 6.py
@@ -0,0 +1,15 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match 
+# the correct Python output.
+
+print ( "{1}AA{0}{1}BB" .format("dog", "cat", 4)) 
+#############################################################
+# print ( 'xx{1:B>5}YY'.format(23))
+#############################################################
+print ( 'xx{0:C<+7}yy'.format(+23)) 
+#############################################################
+print ( "AA{1:x^5}BB".format(123,"dog") ) 
+#############################################################
+print ( "{2}{1}" .format("pet", 2.45, "cat")) 
