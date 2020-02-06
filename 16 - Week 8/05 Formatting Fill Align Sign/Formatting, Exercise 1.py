18eb4a7aeaaa88c85d614174aeac8b190425de32
mohakhalili <md.kh71@gmail.com>
2020-02-06 14:10:28 +0330

    Formatting, Exercise 1 added

---
+++ 16 - Week 8/05 Formatting Fill Align Sign/Formatting, Exercise 1.py
@@ -0,0 +1,15 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match 
+# the correct Python output.
+
+print('X{0:Y<7d}Z'.format(123))
+#########################################################
+print('X{0:Y^9}Z'.format('dog'))
+#########################################################
+print('A{1:C>10.3f}B'.format(12.56,356.1234))
+#########################################################
+print ("A{0:x<8.2f}B".format(12.1234567)) 
+#########################################################
+print ("A{0:x^9.2f}B".format(97.234321))
