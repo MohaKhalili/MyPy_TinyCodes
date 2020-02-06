fac85ec0e680f3bdcdf2ae1eef01a1992afb72a0
mohakhalili <md.kh71@gmail.com>
2020-02-06 14:24:58 +0330

    Formatting, Exercise 4 added

---
+++ 16 - Week 8/05 Formatting Fill Align Sign/Formatting, Exercise 4.py
@@ -0,0 +1,15 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match 
+# the correct Python output.
+
+print('{0:0.2f}'.format(12.12345))
+###################################################################
+print('x{2:}y{1:}z'.format('A','B','C')) 
+###################################################################
+print('x{0:z<10.3f}y'.format(987.654321))
+###################################################################
+print('x{1:z^5}y'.format('one','two'))
+###################################################################
+print('x{0:z^-6d}y'.format(5432))
