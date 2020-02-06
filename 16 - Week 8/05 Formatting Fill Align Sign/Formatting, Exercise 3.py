2723ce10d4cc150137bfbd7b21ff69dc80310062
mohakhalili <md.kh71@gmail.com>
2020-02-06 14:22:24 +0330

    Formatting, Exercise 3 added

---
+++ 16 - Week 8/05 Formatting Fill Align Sign/Formatting, Exercise 3.py
@@ -0,0 +1,15 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match 
+# the correct Python output.
+
+print('A{0:,}B'.format(12345678))
+###############################################################
+# print('A{1:6}B'.format(12)) 
+###############################################################
+print('A{0:C<8.2f}B'.format(12.345678))
+###############################################################
+print('A{0:1.3f}B'.format(12.56789))
+###############################################################
+print('{0:#>6d}'.format(25))
