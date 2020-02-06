f627571c04f9df81a091276f34f5e5fbbc82ccf5
mohakhalili <md.kh71@gmail.com>
2020-02-06 14:26:14 +0330

    Formatting, Exercise 5 added

---
+++ 16 - Week 8/05 Formatting Fill Align Sign/Formatting, Exercise 5.py
@@ -0,0 +1,15 @@
+# What will be printed after each of the following code segments? 
+# If error, then write Error
+
+# Pay attention to the spaces. Your answer should exactly match 
+# the correct Python output.
+
+print('x{0:z>6d}y'.format(5432))
+##############################################################
+print('x{0:z^8d}Y'.format(5432))
+##############################################################
+print('x{0:0.0f}y'.format(2.6789)) 
+##############################################################
+print ('AA{0:+5.3f}BB'.format(123.456789)) 
+##############################################################
+print ('AA{0:C<7}BB'.format(987))
