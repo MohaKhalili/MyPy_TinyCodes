691a02e4b5babcd03ad9cf13b563bb30f0a01666
mohakhalili <md.kh71@gmail.com>
2020-02-29 11:52:43 +0330

    Recursive GCD function added

--- 19 - Week 9/02 Recursion/Recursive Functions Exercise 4 (GCD).py
+++ 19 - Week 9/02 Recursion/Recursive Functions Exercise 4 (GCD).py
@@ -6,13 +6,14 @@
 # Your function should return:
 # 5
 
-# Type your code here
+################### My Solution ###################
 def calculate_gcd(a, b):
     if b==0: 
         return a
     else: 
         return calculate_gcd(b,a%b)
-  
+
+################### Driver Code ###################
 number_a = int(input("Please enter your number_a for exponent calculation : "))
 number_b = int(input("Please enter your number_b for exponent calculation : "))
 result = calculate_gcd(number_a, number_b)
