3638cf6f92dc06c5fd0c0c8627125fa0472efe77
mohakhalili <md.kh71@gmail.com>
2020-02-29 11:51:55 +0330

    Recursive GCD function added

---
+++ 19 - Week 9/02 Recursion/Recursive Functions Exercise 4 (GCD).py
@@ -0,0 +1,19 @@
+# A function named calculate_gcd that receives two positive integers a and b 
+# as parameter and returns their greatest common divisor (GCD) using recursion.
+
+# For example, when your function is called as:
+# calculate_gcd(10, 15)
+# Your function should return:
+# 5
+
+# Type your code here
+def calculate_gcd(a, b):
+    if b==0: 
+        return a
+    else: 
+        return calculate_gcd(b,a%b)
+  
+number_a = int(input("Please enter your number_a for exponent calculation : "))
+number_b = int(input("Please enter your number_b for exponent calculation : "))
+result = calculate_gcd(number_a, number_b)
+print ("The gcd of", number_a, "and", number_b, "is : ",result)
