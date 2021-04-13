a97b5e5e5929e14edb26c369821ae24d1104ea91
mohakhalili <md.kh71@gmail.com>
2021-04-13 13:53:51 +0430

    part6 of exam1 edited

--- 02 - Exam 1/Part6.py
+++ 02 - Exam 1/Part6.py
@@ -3,5 +3,5 @@
 
 # Type your code here
 sample_list = [2, 10, 3, 5]
-mian = (sample_list[0]+sample_list[1]+sample_list[2]+sample_list[3])/4
-print("miangin number is ",mian)
+avg = (sample_list[0]+sample_list[1]+sample_list[2]+sample_list[3])/4
+print(avg)
