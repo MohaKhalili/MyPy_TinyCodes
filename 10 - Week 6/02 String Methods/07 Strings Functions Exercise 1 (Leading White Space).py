628b9315f5500978734041f3801e5f9b7d80f898
mohakhalili <md.kh71@gmail.com>
2021-04-21 16:34:04 +0430

    method 3 for Leading_White_Space added

--- 10 - Week 6/02 String Methods/07 Strings Functions Exercise 1 (Leading White Space).py
+++ 10 - Week 6/02 String Methods/07 Strings Functions Exercise 1 (Leading White Space).py
@@ -27,7 +27,18 @@ def Leading_White_Space2(str):
     
     # slice the string from that index to the end
     str_new = str[index:]
-    return str_new
+    return str_new+
+
+# method 3
+def Leading_White_Space3(my_string):
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != " ":
+            new_string = new_string + my_string[i]
+            if my_string[i+1] == " ":
+                new_string = new_string + my_string[i:]
+                break
+    return new_string
 
 my_str = input("Please enter your string : ")
 result = Leading_White_Space1(my_str)
